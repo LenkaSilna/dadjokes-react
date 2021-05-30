24d036d981b4c11a8aa679fc3fa234e9e791e58b
Lenka Silna <lenka.baricova@post.cz>
2021-05-30 13:04:45 +0200

    ukol 15 a loveni spatne cesty img

--- src/Joke/index.jsx
+++ src/Joke/index.jsx
@@ -1,8 +1,45 @@
 import React, { useState } from 'react';
 import './style.css';
 
-const Joke = () => {
-  return <></>;
+const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
+  const [likeCount, setLikeCount] = useState(0);
+  const [dislikeCount, setDislikeCount] = useState(0);
+  return (
+    <>
+      {' '}
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src={userAvatar} />
+            <p className="user-name">{userName}</p>
+          </div>
+          <p className="joke__text">{text}</p>
+        </div>
+        <div className="joke__likes">
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={() => {
+              setLikeCount(likeCount + 1);
+            }}
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {likeCount}
+          </span>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={() => {
+              setDislikeCount(dislikeCount + 1);
+            }}
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {dislikeCount}
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Joke;
--- src/index.jsx
+++ src/index.jsx
@@ -1,54 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import Joke from './Joke';
+import { jokes } from './jokes';
 
 const App = () => {
-  const [likeCount, setLikeCount] = useState(0);
-  const [dislikeCount, setDislikeCount] = useState(0);
   return (
-    <>
-      <div className="container">
-        <div className="joke">
-          <div className="joke__body">
-            <div className="joke__user">
-              <img
-                className="user-avatar"
-                src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-              />
-              <p className="user-name">Neroxx</p>
-            </div>
-
-            <p className="joke__text">
-              The secret service isn't allowed to yell "Get down!" anymore when
-              the president is about to be attacked. Now they have to yell
-              "Donald, duck!"
-            </p>
-          </div>
-          <div className="joke__likes">
-            <button
-              id="btn-up"
-              className="btn-like btn-like--up"
-              onClick={() => {
-                setLikeCount(likeCount + 1);
-              }}
-            ></button>
-            <span id="likes-up" className="likes-count likes-count--up">
-              {likeCount}
-            </span>
-            <button
-              id="btn-down"
-              className="btn-like btn-like--down"
-              onClick={() => {
-                setDislikeCount(dislikeCount + 1);
-              }}
-            ></button>
-            <span id="likes-down" className="likes-count likes-count--down">
-              {dislikeCount}
-            </span>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="container">
+      {jokes.map((joke) => (
+        <Joke
+          key={joke.id}
+          userAvatar={joke.avatar}
+          userName={joke.name}
+          text={joke.text}
+          likes={joke.likes}
+          dislikes={joke.dislikes}
+        />
+      ))}
+    </div>
   );
 };
 
